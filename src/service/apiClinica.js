257aa41a8cd34f8720b57c90fe3ce5a3005cde6e
RodrigoBatis <rodrigofreirebatista@gmail.com>
2022-12-04 19:05:29 -0300

    .

--- src/service/apiClinica.js
+++ src/service/apiClinica.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClinica = axios.create({
-   baseURL:'http://10.107.144.28:3000'
+   baseURL:'http://192.168.2.108:3000'
 });
 
 export default apiClinica;
--- src/telas/listagem_pacientes.js
+++ src/telas/listagem_pacientes.js
@@ -1,85 +1,130 @@
-import React, { useState, useEffect } from "react";
-import {  Text, 
-          View, 
-          StyleSheet, 
-          SafeAreaView, 
-          ScrollView,
-          Image,
-          TouchableOpacity } from "react-native";
-
-import COLORS from '../const/colors';
-import apiClinica from '../service/apiClinica';
-
-const Listagem = ({navigation})=>{
-
-   const [livros, setLivros] = useState([]);
-
-   useEffect(
-       ()=>{
-           apiLivraria.get('/listarLivros')
-           .then(
-               (data)=>{
-                   console.log(data.data);
-                   setLivros(data.data);
-               }
-           )
-       },
-       []
-   );
-
-   return(
-               <ScrollView>
-
-                   <View style={estilos.container}>
-
-                       {
-                           livros.map(
-                               livro=>(
-                                   <TouchableOpacity
-                                       key={livro.cod_livro}
-                                       style={estilos.post}
-                                       onPress={()=>navigation.navigate('Detalhes', {cod_livro:livro.cod_livro})}>
-
-                                       <View>
-                                           <Image style={estilos.imagem} source={capaLivro150}/>
-                                           <Text style={estilos.titulo}>{livro.titulo}</Text>
-                                       </View>
-
-                                   </TouchableOpacity>
-                               )
-                           )
-                       }
-
-                   </View>
-
-               </ScrollView>
-   );
-
-}
-
-const estilos = StyleSheet.create({
-   container:{
-       alignItems:'center'
-   },
-   post:{
-       width:'95%',
-       alignItems:'center',
-       backgroundColor:'#CCC',
-       padding:15,
-       marginVertical:5,
-       borderRadius:5,
-       elevation:5,
-   },
-   imagem:{
-       borderRadius:5,
-       marginVertical:16,
-       marginLeft:16
-   },
-   titulo:{
-       textAlign:'center',
-       fontSize:20,
-       fontWeight:'bold'
-   }
-});
-
-export default Listagem;
+import {
+    View,
+    Text,
+    ScrollView,
+    TouchableOpacity,
+    StyleSheet,
+    SafeAreaView,
+  } from "react-native";
+  import React, { useState, useEffect } from "react";
+  import apiClinica from "../service/apiClinica";
+  import COLORS from "../const/colors";
+  import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+  
+  const Listagem = () => {
+    // Pegando o valor do cod_paciente
+    const [paciente, setPaciente] = useState([]);
+  
+    useEffect(() => {
+      apiClinica.get("/listarPacientes").then((paciente) => {
+        console.log(paciente);
+        setPaciente(paciente.data);
+      });
+    }, []);
+  
+    return (
+      <ScrollView style={{ backgroundColor: COLORS.dark_blue }}>
+        <SafeAreaView style={estilos.container}>
+          <View style={estilos.containerPaciente}>
+            {paciente.map((paciente) => (
+              <TouchableOpacity
+                key={paciente.cod_paciente}
+                style={estilos.post}
+                onPress={() =>
+                  navigation.navigate("Detalhes", {
+                    cod_paciente: paciente.cod_paciente,
+                  })
+                }
+              >
+                <View style={estilos.cardPaciente}>
+                  {/* <Icon size={70} color={COLORS.branco}/> */}
+                  <View style={estilos.paciente}>
+                    <Icon
+                      name="account-circle"
+                      size={70}
+                      style={{ marginEnd: 18 }}
+                    />
+                    <View style={{ flex: 1, width: "100%" }}>
+                      {/* Nome */}
+                      <Text>Nome Paciente</Text>
+                      <Text style={estilos.titulo}>{paciente.nome_paciente}</Text>
+                      {/* Email */}
+                      <View style={estilos.caixaTexto}>
+                        <Text>E-mail Paciente</Text>
+                        <Text style={estilos.titulo}>
+                          {paciente.email_paciente}
+                        </Text>
+                      </View>
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: "row", width: "100%" }}>
+                    <View style={[{ marginRight: 50 }, estilos.caixaTexto]}>
+                      <Text>Telefone Paciente</Text>
+                      <Text style={estilos.titulo}>
+                        {paciente.telefone_paciente}
+                      </Text>
+                    </View>
+                    <View style={estilos.caixaTexto}>
+                      <Text>Celular Paciente</Text>
+                      <Text style={estilos.titulo}>
+                        {paciente.celular_paciente}
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={estilos.caixaTexto}>
+                    <Text>Nome Responsável</Text>
+                    <Text style={estilos.titulo}>
+                      {paciente.nome_responsavel}
+                    </Text>
+                  </View>
+                  <View style={estilos.caixaTexto}>
+                    <Text>Telefone Responsável</Text>
+                    <Text style={estilos.titulo}>
+                      {paciente.telefone_responsavel}
+                    </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </SafeAreaView>
+      </ScrollView>
+    );
+  };
+  
+  const estilos = StyleSheet.create({
+    container: {
+      flex: 1,
+      height: "100%",
+      width: "100%",
+      marginTop: 100,
+    },
+    containerPaciente: {
+      flex: 1,
+      justifyContent: "center",
+      paddingStart: "5%",
+      paddingEnd: "5%",
+    },
+    cardPaciente: {
+      backgroundColor: COLORS.light,
+      height: 300,
+      marginBottom: 30,
+      borderRadius: 20,
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+    },
+    titulo: {
+      color: COLORS.azulEscuro,
+      borderBottomColor: COLORS.dark_blue,
+      borderBottomWidth: 1,
+    },
+    paciente: {
+      flexDirection: "row",
+      flexWrap: "wrap",
+    },
+    caixaTexto: {
+      marginTop: 10,
+    },
+  });
+  
+  export default Listagem;
